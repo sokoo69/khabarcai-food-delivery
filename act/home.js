9c4fca9ea86d4dbe6d3277a7194768499d5ca810
saykot biswas shawon <shawon.saykot2023@gmail.com>
2025-09-29 00:49:16 +0600

    home

---
+++ act/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to KhabarCai</h1>
+            <p>Your source for the latest news and updates.</p>
+        </div>
+    );
+}
